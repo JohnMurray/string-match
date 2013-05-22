0e76cb281b082fded4e8d653d013c16e200f86bd
John Murray <me@johnmurray.io>
2013-05-22 08:04:45 -0400

    Toying and Notes
    Updated some of the documentation for the qs_score
    algorithm but I have yet to really write the algorithm.
    Also, I played around a bit with the assert! macro,
    although there's not much in the test file as of right
    now. Anyways, just commiting this before I forget and it
    ends up going in with some actually useful stuff.

---
+++ src/string_match.rs
@@ -0,0 +1,27 @@
+/**
+ * Public: Using the QuickSilver algorithm, determine the match between two
+ *         strings. Algorithm orginally ported from Lachie Cox's implementation
+ *         for jQuery.
+ * 
+ * str1 -         The string that is being compared _against_ (the baseline 
+ *                string). If this were called in an OO fashion, it would look
+ *                like:
+ *                    str1.qs_match(abbreviation, offset);
+ * abbreviation - The string that is being checked for match-ness to str1
+ * offset -       The point in the string from which the match checking(?) will
+ *                start, typically zero.
+ *
+ * Notes: QS jQuery: http://static.railstips.org/orderedlist/demos/quicksilverjs/javascripts/quicksilver.js
+ *
+ * Returns a f32 
+ */
+fn qs_score(str1 : &str, abbreviation: &str, offset : i16) -> f32 {
+    if str::len(abbreviation) == 0 { return 0.9f32; }
+    if str::len(abbreviation) > str::len(str1) { return 0.0f32; }
+
+    for uint::range(0, str::len(abbreviation) - 1) |i| {
+        
+    }
+
+    0.0f32
+}
---
+++ test/string_match.rs
@@ -0,0 +1,6 @@
+use string_match;
+
+#[test]
+fn qs_should_return_09_for_empty_comprator_string() {
+    assert!(true);
+}
